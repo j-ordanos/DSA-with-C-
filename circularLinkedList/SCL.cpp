d255cd6fb24f3f958042c3334c94d4fec099969e
j-ordanos <151696220+j-ordanos@users.noreply.github.com>
2024-11-13 07:04:57 +0300

    Update SCL.cpp

--- circularLinkedList/SCL.cpp
+++ circularLinkedList/SCL.cpp
@@ -1,51 +1,53 @@
 #include <iostream>
 using namespace std;
 
-struct studentNode{
-	int id;
-	string name;
-	studentNode* next;
+struct studentNode {
+    int id;
+    string name;
+    studentNode* next;
 };
 
-void display(studentNode* head){
-	studentNode* temp = head;
-	if(head == NULL){
-		cout << "List is empty"<<endl;
-		return;
-	}
-	do{
-		cout << "ID: "<<temp->id<<" "<<"Name: "<<temp->name<<endl;
-		temp = temp->next;	
-	}while(temp != head);
+void display(studentNode* head) {
+    studentNode* temp = head;
+    if (head == NULL) {  
+        cout << "List is empty" << endl;
+        return;
+    }
+    do {
+        cout << "ID: " << temp->id << ", Name: " << temp->name << endl;  // Display current node's data
+        temp = temp->next;  
+    } while (temp != head);  // Loop until back at the head node
 }
 
-int main(){
-	int num;
-	cout << "Enter number of students: ";
-	cin >> num;
-	
-	studentNode* head = NULL;
-	studentNode* current = NULL;
-	
-	cout << "Entering data..."<<endl;
-	for(int i=0; i < num; i++){
-		studentNode* newNode = new studentNode();
-		cout << "Student "<<i+1<<endl;
-		cout << "Id: ";
-		cin >> newNode->id;
-		cout << "Name: ";
-		cin >> newNode->name;
-		newNode->next = NULL;
-		
-		if(head == NULL){
-			head = current = newNode;
-		}else{
-			current->next = newNode;
-			current = newNode;	
-		}
-	}
-		current->next = head;
-		
-	cout << "\nBefore:"<<endl;
-	display(head);
+int main() {
+    int num;
+    cout << "Enter number of students: ";
+    cin >> num;
+
+    studentNode* head = NULL;
+    studentNode* current = NULL;
+
+    cout << "Entering data..." << endl;
+    for (int i = 0; i < num; i++) {
+        studentNode* newNode = new studentNode();  // Allocate memory for a new node
+        cout << "Student " << i + 1 << endl;
+        cout << "Id: ";
+        cin >> newNode->id;
+        cout << "Name: ";
+        cin >> newNode->name;
+        newNode->next = NULL;
+
+        if (head == NULL) {  // If list is empty, initialize head
+            head = current = newNode;
+        } else {
+            current->next = newNode;  // Link current node to the new node
+            current = newNode;  // Update current to the new node
+        }
+    }
+    current->next = head;  // Make the list circular by linking last node to head
+
+    cout << "\nDisplaying the list..." << endl;
+    display(head);  // Display the circular linked list
+
+    return 0;
 }
